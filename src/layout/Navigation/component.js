1f356a020f8d11a82056694b18cbe3c51f33885b
pmita <panos.mita2@outlook.com>
2022-09-02 15:57:11 +0100

    responsive, and togglable menu

--- src/layout/Navigation/component.js
+++ src/layout/Navigation/component.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+// ROUTER
 import { Link } from 'react-router-dom';
 // ASSETS
 import burgerIcon from '../../assets/images/svg/burger.svg';
@@ -5,14 +7,19 @@ import burgerIcon from '../../assets/images/svg/burger.svg';
 import './style.scss';
 
 function Navigation() {
+  // STATE & VARIABLES
+  const [isHidden, setIsHidden] = useState(false);
+
   return (
     <nav>
       <h4 id="nav-logo">HandymanInNewcastle</h4>
-      <ul className="nav-items active">
+      <ul className={isHidden ? 'nav-items active' : 'nav-items'}>
         <li><Link to="/">Work</Link></li>
         <li><Link to="/">Get in Touch</Link></li>
       </ul>
-      <img className="nav-burger" src={burgerIcon} alt="toggle menu icon" />
+      <button className="nav-burger" type="button" onClick={() => setIsHidden(!isHidden)}>
+        <img src={burgerIcon} alt="toggle menu icon" />
+      </button>
     </nav>
   );
 }
