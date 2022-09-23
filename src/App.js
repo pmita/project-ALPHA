b81c7b72d565add089a1918ece2e322283583384
pmita <panos.mita2@outlook.com>
2022-09-23 14:29:11 +0100

    guarding our private routes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import { useEffect } from 'react';
 // ROUTER
 import { Routes, Route } from 'react-router-dom';
 // REDUX
-import { useDispatch } from 'react-redux';
-import { authenticated } from './app/slices/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { authenticated, selectAuthUser } from './app/slices/authSlice';
+// FIREBASE
+import { fireAuth } from './firebase/config';
 // COMPONENTS
 import Navigation from './layout/Navigation';
+import PrivateRouteToHome from './components/RouteGuards/PrivateRouteToHome';
+import PrivateRouteToDashboard from './components/RouteGuards/PrivateRouteToDashboard';
 // PAGES
 import Home from './pages/Home';
 import OurWork from './pages/OurWork/component';
 import Contact from './pages/Contact/component';
 import SignIn from './pages/SignIn/component';
 import Dashboard from './pages/Dashboard/component';
-import { fireAuth } from './firebase/config';
 
 function App() {
   // STATE & VARIABLES
   const dispatch = useDispatch();
+  const user = useSelector(selectAuthUser);
 
-  // USE EFFECT
+  // useEFFECT
   useEffect(() => {
-    const unsubscribe = fireAuth.onAuthStateChanged((user) => {
-      dispatch(authenticated(user));
+    const unsubscribe = fireAuth.onAuthStateChanged((refUser) => {
+      dispatch(authenticated(refUser));
     });
 
     return () => unsubscribe();
@@ -34,8 +38,12 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/our-work" element={<OurWork />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/admin/signin" element={<SignIn />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/signin" elemement={<PrivateRouteToHome user={user} />}>
+          <Route path="/admin/signin" element={<SignIn />} />
+        </Route>
+        <Route path="/admin/dashboard" element={<PrivateRouteToDashboard user={user} />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </div>
   );
