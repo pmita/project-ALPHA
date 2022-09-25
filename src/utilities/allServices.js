e290e17e771fa7c29c132f1e298691ac226c3624
pmita <panos.mita2@outlook.com>
2022-09-25 16:20:27 +0100

    adding svgs

--- src/utilities/allServices.js
+++ src/utilities/allServices.js
@@ -2,26 +2,26 @@
 export const allServices = [
   {
     id: 1,
-    img: '/assets/images/svg/pound-symbol.svg',
-    altText: 'Pound symbol',
+    img: '/assets/images/svg/crown-symbol.svg',
+    altText: 'King crown symbol',
     title: 'Luxury Facilitates',
-    description: 'The advantage of hiring a workspace with us is that gives you comfortable services',
-    learnMore: 'This is some dummy text for our service for now'
+    description: 'The advantage of hiring us is that gives you comfortable services',
+    learnMore: 'We are more than heppy to work around your schedule and deadlines. Let us know about key date and we will take it from there; and if anything changes we are only a phone call away.'
   },
   {
     id: 2,
     img: '/assets/images/svg/pound-symbol.svg',
     altText: 'Pound symbol',
-    title: 'Luxury Facilitates',
-    description: 'The advantage of hiring a workspace with us is that gives you comfortable services',
-    learnMore: 'This is some dummy text for our service for now'
+    title: 'Affordable Price',
+    description: 'You can get a solution of the highest quality at an affordable price',
+    learnMore: 'We do things like we want things to be done in our own homes and we do everything in our power to meet our customer expectations.'
   },
   {
     id: 3,
-    img: '/assets/images/svg/pound-symbol.svg',
+    img: '/assets/images/svg/chart-symbol.svg',
     altText: 'Pound symbol',
-    title: 'Luxury Facilitates',
-    description: 'The advantage of hiring a workspace with us is that gives you comfortable services',
-    learnMore: 'This is some dummy text for our service for now'
+    title: 'Smooth Workflow',
+    description: 'We provide the most easy smooth workflow for our clients',
+    learnMore: 'We handle everything, beginning to end. If you encounter any setbacks we are here to talk about and get over them, together.'
   }
 ];
